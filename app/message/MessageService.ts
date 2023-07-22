4f0b5f825abf7e2507ba3d608ab66b2c025050c0
Balazs Nyirfa <balazsnyirfa@gmail.com>
2023-07-22 22:29:03 +0200

    add new message methods in MessageService

--- app/message/MessageService.ts
+++ app/message/MessageService.ts
@@ -1,10 +1,11 @@
 import { ChatCompletionRequestMessageRoleEnum } from "openai";
 import { GameInstance } from "../gameInstance/models/GameInstance";
+import * as GameInstanceService from "./../gameInstance/GameInstanceService";
 import { AppDataSource } from "./../global/data-source";
 import { Message } from "./models/Message";
 
 export const createMessage = async (
-  gameId: number,
+  gameId: string,
   role: ChatCompletionRequestMessageRoleEnum,
   content: string
 ) => {
@@ -27,11 +28,43 @@ export const createMessage = async (
   }
 };
 
-export const getMessagesByGameInstance = async (gameId: number) => {
+export const deleteMessageById = async (messageId: string) => {
+  try {
+    // delete message
+    const messageRepository = AppDataSource.getRepository(Message);
+    await messageRepository.delete({ id: messageId });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error in 'MessageService.deleteMessageById'");
+  }
+};
+
+export const updateMessageById = async (
+  messageId: string,
+  role: ChatCompletionRequestMessageRoleEnum,
+  content: string,
+  timestamp: Date
+) => {
+  try {
+    // update message
+    const messageRepository = AppDataSource.getRepository(Message);
+    await messageRepository.update(messageId, {
+      role: role,
+      content: content,
+      timestamp: timestamp,
+    });
+    // return the updated message
+    return await messageRepository.findOneBy({ id: messageId });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error in 'MessageService.updateMessageById'");
+  }
+};
+
+export const getMessagesByGameInstance = async (gameId: string) => {
   try {
     // get gameInstance by gameId
-    const gameInstanceRepository = AppDataSource.getRepository(GameInstance);
-    const gameInstance = await gameInstanceRepository.findOneBy({ id: gameId });
+    const gameInstance = await GameInstanceService.getGameInstanceById(gameId);
     // get messages
     const messageRepository = AppDataSource.getRepository(Message);
     const messages = await messageRepository.find({
@@ -54,5 +87,44 @@ export const getMessages = async () => {
   } catch (error) {
     console.error(error);
     throw new Error("Error in 'MessageService.getMessages'");
+  }
+};
+
+export const getSystemMessagesByGameInstance = async (gameId: string) => {
+  try {
+    // get gameInstance by gameId
+    const gameInstance = await GameInstanceService.getGameInstanceById(gameId);
+    // get messages
+    const messageRepository = AppDataSource.getRepository(Message);
+    const messages = await messageRepository.find({
+      where: {
+        gameInstance: gameInstance,
+        role: ChatCompletionRequestMessageRoleEnum.System,
+      },
+      order: { timestamp: "ASC" },
+    });
+    return messages;
+  } catch (error) {
+    console.error(error);
+    throw new Error(
+      "Error in 'MessageService.getSystemMessagesByGameInstance'"
+    );
+  }
+};
+
+export const getSystemMessages = async () => {
+  try {
+    // get messages
+    const messageRepository = AppDataSource.getRepository(Message);
+    const messages = await messageRepository.find({
+      where: {
+        role: ChatCompletionRequestMessageRoleEnum.System,
+      },
+      order: { timestamp: "ASC" },
+    });
+    return messages;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error in 'MessageService.getSystemMessages'");
   }
 };
