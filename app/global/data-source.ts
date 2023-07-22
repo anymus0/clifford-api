918317b95e1aeb913921b15e808ad0363e0b398c
Balazs Nyirfa <balazsnyirfa@gmail.com>
2023-07-22 20:12:20 +0200

    add DB_LOGGINF env variable

--- app/global/data-source.ts
+++ app/global/data-source.ts
@@ -1,22 +1,22 @@
 // import packages
 import { env } from "process";
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 import { Message } from "../message/models/Message";
 // import entities
-import { GameInstance } from "./../gameInstance/models/GameInstance"
+import { GameInstance } from "./../gameInstance/models/GameInstance";
 
 // create db data source
 export const AppDataSource = new DataSource({
-    type: "mariadb",
-    host: "db",
-    port: 3306,
-    username: env.DB_USERNAME || "clifford",
-    password: env.DB_PASSWORD || "123",
-    database: env.DB_NAME || "CLIFFORDAPP",
-    synchronize: true,
-    logging: false,
-    entities: [GameInstance, Message],
-    migrations: [],
-    subscribers: [],
-})
+  type: "mariadb",
+  host: "db",
+  port: 3306,
+  username: env.DB_USERNAME || "clifford",
+  password: env.DB_PASSWORD || "123",
+  database: env.DB_NAME || "CLIFFORDAPP",
+  synchronize: true,
+  logging: env.DB_LOGGING?.toLowerCase() === 'true',
+  entities: [GameInstance, Message],
+  migrations: [],
+  subscribers: [],
+});
